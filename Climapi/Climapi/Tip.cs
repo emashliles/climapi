804873c66dc5a3013a72c9d23ef444498cc6711d
emashliles <emashliles@gmail.com>
2014-10-11 15:26:25 +0100

    :)

---
+++ Climapi/Climapi/Tip.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Climapi
+{
+    public class Tip
+    {
+        public string How { get; set; }
+        public string Why { get; set; }
+        public string Tag { get; set; }
+
+        public Tip(string how, string why, string tag)
+        {
+            How = how;
+            Why = why;
+            Tag = tag;
+        }
+    }
+}
